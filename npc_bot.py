3007a36cb5fe5e91a61071dff3ad534568f6872a
crash0425 <mlanning04@gmail.com>
2025-04-17 22:12:40 -0400

    Update npc_bot.py

--- npc_bot.py
+++ npc_bot.py
@@ -39,8 +39,10 @@ TRIVIA_AND_LORE = [
 ]
 
 # --- Post to Facebook ---
-def post_to_facebook(npc, image_path=None):
-    print("🔍 Debug: FB_PAGE_ID:", os.getenv("FB_PAGE_ID"))
+ef post_to_facebook(npc, image_path=None):
+    refresh_facebook_token()  # <<< ADD THIS LINE FIRST!
+
+    print("🔍 Debug: FB_PAGE_ID:", os.getenv("FB_PAGE_ID
     print("🔍 Debug: FB_PAGE_ACCESS_TOKEN present:", bool(os.getenv("FB_PAGE_ACCESS_TOKEN")))
     page_id = os.getenv("FB_PAGE_ID")
     token = os.getenv("FB_PAGE_ACCESS_TOKEN")
