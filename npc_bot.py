68fed882fc90f9848c3451c2dff324882a696311
crash0425 <mlanning04@gmail.com>
2025-04-22 16:34:25 -0400

    Update npc_bot.py

--- npc_bot.py
+++ npc_bot.py
@@ -28,22 +28,6 @@ next_scheduled_time = None
 next_scheduled_day = None
 VERIFY_TOKEN = os.getenv("FB_VERIFY_TOKEN")
 
-def post_to_facebook(image_path, message):
-    url = f"https://graph.facebook.com/v19.0/me/photos"
-    payload = {
-        "caption": message,
-        "access_token": PAGE_ACCESS_TOKEN
-    }
-    files = {
-        "source": open(image_path, "rb")
-    }
-    response = requests.post(url, data=payload, files=files)
-    
-    if response.status_code == 200:
-        print("✅ Successfully posted to Facebook!")
-    else:
-        print(f"❌ Failed to post to Facebook: {response.text}")
-
 # --- Settings
 ARCHIVE_FILE = "npc_archive.txt"
 VOLUME_FOLDER = "npc_volumes"
